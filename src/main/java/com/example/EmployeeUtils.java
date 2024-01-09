380e4f3b291eeddb27ac960c371b79280d84ebf6
locpham97 <loc.pham@codeenginestudio.com>
2024-01-09 11:44:35 +0700

    working with excercise

--- src/main/java/com/example/EmployeeUtils.java
+++ src/main/java/com/example/EmployeeUtils.java
@@ -6,27 +6,32 @@ import java.util.function.Predicate;
 public class EmployeeUtils {
 
   public String getFullName(Employee employee) {
-    Function<Employee, String> fullName = null;
+    Function<Employee, String> fullName = (e) ->{
+
+      return e.getFirstName() + " " + e.getLastName();
+    };
     return fullName.apply(employee);
   }
 
   public String getManagersLastName(Employee employee) {
-    Function<Employee, String> managersLastName = null;
+    Function<Employee, String> managersLastName = employee1 -> {
+      return employee1.getManager().getLastName();
+    };
     return managersLastName.apply(employee);
   }
 
   public boolean hasBeenEmployedLongerThanFiveYears(Employee employee) {
-    Predicate<Employee> employedLongerThanFiveYears = null;
+    Predicate<Employee> employedLongerThanFiveYears = (e -> e.getYearsOfService() > 5);
     return employedLongerThanFiveYears.test(employee);
   }
 
   public boolean hasMoreThanThreeDirectReports(Employee employee) {
-    Predicate<Employee> moreThanThreeDirectReports = null;
+    Predicate<Employee> moreThanThreeDirectReports = e -> e.getNumberOfDirectReports() > 3;
     return moreThanThreeDirectReports.test(employee);
   }
 
   public boolean hasMoreThanThreeYearsOfServiceAndLessThanTwoDirectReports(Employee employee) {
-    Predicate<Employee> moreThanThreeYearsOfServiceAndLessThanTwoDirectReports = null;
+    Predicate<Employee> moreThanThreeYearsOfServiceAndLessThanTwoDirectReports = e -> e.getYearsOfService() > 3 &&  e. getNumberOfDirectReports() < 2;
     return moreThanThreeYearsOfServiceAndLessThanTwoDirectReports.test(employee);
   }
 }
